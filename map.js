a11f6d5578a5f860bcbeea45d6dcf39ced9641eb
Md-coder <davidmadu24@gmail.com>
2021-11-22 10:54:21 +0100

    input ui

--- map.js
+++ map.js
@@ -1,5 +1,8 @@
 let geocoder;
-// call action button
+// input value
+
+// create a listener
+let search = document.getElementById("button");
 
 function initMap() {
   // setting the map options
@@ -16,20 +19,48 @@ function initMap() {
   let map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   // listen for click
-  google.maps.event.addEventListener(map, "click", (event) => {
-    // add marker
-    addMarker({ location: event.latlng });
+  google.maps.event.addListener(map, "click", (event) => {
+    // Add marker
+    addMarker({ location: event.latLng });
   });
 
-  // make an array for the markers
-  let markerArray = [
-    { location: { lat: 6.864472, lng: 7.414 }, imageIcon: `<i class="fa-solid fa-flag"></i>` },
-    { location: { lat: 6.773175, lng: 7.433683 }, imageIcon: `<i class="fa-solid fa-flag"></i>` },
-  ];
+  search.addEventListener("click", () => {
+    let latVal = document.getElementById("latitudeVal").value;
+    let longVal = document.getElementById("longVal").value;
 
-  // loop to add marker
-  for (let i = 0; i < markerArray.length; i++) {
-    addMarker(markerArray[i]);
+    // create new marker
+    let newMarker = {
+      location: { lat: parseInt(latVal), lng: parseInt(longVal) },
+      imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+    };
+    // push the new marker into the array
+    addNewMarker(newMarker);
+    console.log(parseFloat(longVal));
+  });
+
+  //  create a function for adding new markerArray
+  function addNewMarker(newMarker) {
+    // make an array for the markers
+    let markerArray = [
+      {
+        location: { lat: 6.864472, lng: 7.414 },
+        imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      },
+      {
+        location: { lat: 6.773175, lng: 7.433683 },
+        imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      },
+      {
+        location: { lat: 6.871623, lng: 7.419083 },
+        imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      },
+    ];
+
+    markerArray.push(newMarker);
+    // loop to add marker
+    for (let i = 0; i < markerArray.length; i++) {
+      addMarker(markerArray[i]);
+    }
   }
 
   // add marker
