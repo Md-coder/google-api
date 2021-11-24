5fe2be960b3084f771350f1f919ac2c4f745dbe2
Md-coder <davidmadu24@gmail.com>
2021-11-24 13:37:50 +0100

    added label and a label input

--- map.js
+++ map.js
@@ -21,29 +21,31 @@ function initMap() {
   search.addEventListener("click", () => {
     let latVal = document.getElementById("latitudeVal").value;
     let longVal = document.getElementById("longVal").value;
+    let sample = document.getElementById("sampleVal").value;
 
     // create new marker
     let newMarker = {
       location: { lat: parseFloat(latVal), lng: parseFloat(longVal) },
       imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      content: `<p>${sample}</p>`,
     };
 
     //  create a function for adding new markerArray
 
     // make an array for the markers
     let markerArray = [
-      {
-        location: { lat: 6.864472, lng: 7.414 },
-        imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
-      },
-      {
-        location: { lat: 6.773175, lng: 7.433683 },
-        imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
-      },
-      {
-        location: { lat: 6.871623, lng: 7.419083 },
-        imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
-      },
+      // {
+      //   location: { lat: 6.864472, lng: 7.414 },
+      //   imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      // },
+      // {
+      //   location: { lat: 6.773175, lng: 7.433683 },
+      //   imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      // },
+      // {
+      //   location: { lat: 6.871623, lng: 7.419083 },
+      //   imageIcon: "https://img.icons8.com/nolan/1x/marker.png",
+      // },
     ];
     // push the new marker into the array
     markerArray.push(newMarker);
@@ -65,6 +67,14 @@ function initMap() {
       if (value.imageIcon) {
         // set icon
         marker.setIcon(value.imageIcon);
+      }
+
+      if (value.content) {
+        var infoWindow = new google.maps.InfoWindow({
+          content: value.content,
+        });
+
+        infoWindow.open(map, marker);
       }
     }
 
